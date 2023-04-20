c91ff89b377f3093d57388f859ed6d7d3bd7c725
Lightl707 <Lightl707@mail.ru>
2023-04-20 16:41:26 +0600

    upd

---
+++ Lab10/hhfront/src/app/companies/companies.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../models';
+import { CompanyService } from '../company.service';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css']
+})
+export class CompaniesComponent implements OnInit{
+  companies: Company[] = [];
+  constructor(private service: CompanyService){}
+  ngOnInit(): void{
+
+    this.service.getCompanies().subscribe((companies)=>{
+      this.companies = companies
+    })
+    console.log(this.companies)
+  }
+}
